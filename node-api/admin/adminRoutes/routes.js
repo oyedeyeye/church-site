d62c352bce83d34cfff8c5f819fda0ee5fb92418
Hezekiah Oyemade <oyedey@gmail.com>
2023-12-23 12:24:04 +0100

    admin route update

--- node-api/admin/adminRoutes/routes.js
+++ node-api/admin/adminRoutes/routes.js
@@ -20,17 +20,17 @@ setLogLevel("info");
 // Private Routes for files
 
 /** Upload Blob files ========================== */
-router.post('/admin/upload', userAuth.authenticateRequest, async (request, response) => await uploadMessage(request, response));
+router.post('/upload', userAuth.authenticateRequest, async (request, response) => await uploadMessage(request, response));
 
 /** DELETE single message Entry ========================== */
-router.delete('/admin/:partitionKey/:rowKey/:filename', userAuth.authenticateRequest, async (request, response) => await deleteMessage(request, response));
+router.delete('/:partitionKey/:rowKey/:filename', userAuth.authenticateRequest, async (request, response) => await deleteMessage(request, response));
 
 
 /** Update single message Entry ========================== */
-router.put('/admin/update-entity/:partitionKey/:rowKey', userAuth.authenticateRequest, async (request, response) => await updateMessage(request, response));
+router.put('/update-entity/:partitionKey/:rowKey', userAuth.authenticateRequest, async (request, response) => await updateMessage(request, response));
 
 /** Read multiple message Entry ========================== */
-router.get('admin/dashboard', userAuth.authenticateRequest, async (request, response) => await resources(request, response));
+router.get('/dashboard', userAuth.authenticateRequest, async (request, response) => await resources(request, response));
 
 
 
