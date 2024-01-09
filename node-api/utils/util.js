d60c8685ec4310e5c6f1f6e816231b696e95a6fd
Hezekiah Oyemade <oyedey@gmail.com>
2024-01-09 21:44:41 +0100

    update function and folder structure

--- node-api/utils/util.js
+++
@@ -1,8 +0,0 @@
-// Create reverse timestamps for the rowKey
-const createReverseTimeStamp = () => {
-  const maxTimestamp = new Date(9999, 11, 31).getTime(); // December 31, 9999)
-  return (maxTimestamp - Date.now()).toString();
-}
-
-
-module.exports = createReverseTimeStamp;
