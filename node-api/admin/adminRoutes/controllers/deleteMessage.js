d956b13f34930bd5c8b04bfa6c8ccaed6a2a7ef7
Hezekiah Oyemade <oyedey@gmail.com>
2024-03-26 16:33:20 +0100

    refactor delete module

--- node-api/admin/adminRoutes/controllers/deleteMessage.js
+++ node-api/admin/adminRoutes/controllers/deleteMessage.js
@@ -6,18 +6,18 @@ const deleteMessage = async (request, response) => {
   try {
     // pass the partitionKey and the rowkey of the file
     // to initiate the delete operation
-    const { partitionKey, rowKey, fileName } = request.params;
+    const { partitionKey, rowKey, fileName } = request.query;
 
-
+    response.status(200).send({message: 'Starting the delete process'});
     // delete the entity with the partitionKey and rowKey from the Azure table
     await mainTable.deleteRecord({ partitionKey, rowKey });
 
     // delete the files from the Blob containers
     // TO DO
     // Trim the file name to have it as the blobName
-    const audioClient = storageContainer.audioClient,
-          pdfClient = storageContainer.pdfClient,
-          imageClient = storageContainer.imageClient;
+    const audioClient = storageContainer.audioClient(),
+          pdfClient = storageContainer.pdfClient(),
+          imageClient = storageContainer.imageClient();
 
     await Promise.all([
       storageContainer.deleteBlob(audioClient, `${fileName}`),
