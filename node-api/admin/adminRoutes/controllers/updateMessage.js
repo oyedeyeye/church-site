61e9091144e1199801032ad164fae7d0cca2f06b
Hezekiah Oyemade <oyedey@gmail.com>
2024-03-26 16:33:47 +0100

    refactor update module

--- node-api/admin/adminRoutes/controllers/updateMessage.js
+++ node-api/admin/adminRoutes/controllers/updateMessage.js
@@ -4,84 +4,83 @@ const storageContainer = require("../../../utils/storage");
 
 const updateMessage = async (request, response) => {
   try {
-    const { partitionKey, rowKey } = request.params;
+    const { partitionKey, rowKey } = request.query;
+
+    if (!partitionKey || !rowKey) {
+      return response.status(400).send({ message: 'Missing required parameters' });
+    }
+
+    response.status(200).send({message: 'Starting the update process'});
+    // Assign the payload to be updated
     const updateData = request.body;
 
     // Fetch the existing entity
     const entity = await mainTable.readRecord({ partitionKey, rowKey });
+    // log the entity to the console
+    console.log(entity);
+    // Defining the fileBlobName from the entity
+    let fileBlobName;
 
-    // Defining the blobName from the entity
-    let blobName;
-
-    if (entity.audioFile === entity.pdfFile || entity.audioFile === entity.messageThumbnail) {
-      blobName = entity.audioFile ? entity.audioFile : entity.pdfFile;
+    if (entity.audioFile || entity.pdfFile) {
+      fileBlobName = entity.pdfFile ? entity.pdfFile.split('.')[0] : entity.audioFile.split('.')[0];
     }
 
     // check if blob names are updated and handled accordingly
     // message thumbnail blob
     if (entity.messageThumbnail !== updateData.messageThumbnail && updateData.messageThumbnail !== null) {
       // Delete the old blob
-      await storageContainer.deleteBlob(storageContainer.imageClient, entity.messageThumbnail);
+      await storageContainer.deleteBlob(storageContainer.imageClient(), entity.messageThumbnail);
 
       // Handle uploading of the updated file to blob
       await storageContainer.uploadBlobToContainer(
-        storageContainer.imageClient, 
+        storageContainer.imageClient(), 
         updateData.messageThumbnail,
-        blobName,
-        (name) => {
-          updateData['messageThumbnail'] = name;
-        },
-        (err) => {
+        fileBlobName)
+        .then(() => updateData['messageThumbnail'] = fileBlobName)
+        .catch((err) => {
           response.status(500).json({
             message: 'Message thumbnail update failed',
             data: err.message,
           });
-        }
-        );
+        });
     }
 
     // audio file blob
     if (entity.audioFile !== updateData.audioFile && updateData.audioFile !== null) {
       // Delete the old blob
-      await storageContainer.deleteBlob(storageContainer.audioClient, entity.audioFile);
+      await storageContainer.deleteBlob(storageContainer.audioClient(), entity.audioFile);
 
       // Handle uploading of the updated file to blob
       await storageContainer.uploadBlobToContainer(
-        storageContainer.audioClient, 
+        storageContainer.audioClient(), 
         updateData.audioFile,
-        blobName,
-        (name) => {
-          updateData['audioFile'] = name;
-        },
-        (err) => {
+        fileBlobName)
+        .then(() => updateData['audioFile'] = fileBlobName)
+        .catch((err) => {
           response.status(500).json({
             message: 'Audio file upload failed',
             data: err.message,
           });
-        }
-        );
+        });
     }
 
     // pdf file blob
     if (entity.pdfFile !== updateData.pdfFile && updateData.pdfFile !== null) {
       // Delete the old blob
-      await storageContainer.deleteBlob(storageContainer.pdfClient, entity.pdfFile);
+      await storageContainer.deleteBlob(storageContainer.pdfClient(), entity.pdfFile);
 
       // Handle uploading of the updated file to blob
       await storageContainer.uploadBlobToContainer(
-        storageContainer.pdfClient, 
+        storageContainer.pdfClient(), 
         updateData.pdfFile,
-        blobName,
-        (name) => {
-          updateData['pdfFile'] = name;
-        },
-        (err) => {
+        fileBlobName)
+        .then(() => updateData['pdfFile'] = fileBlobName)
+        .catch((err) => {
           response.status(500).json({
             message: 'PDF file upload failed',
             data: err.message,
           });
-        }
-        );
+        });
     }
 
     // update the entity with new data
