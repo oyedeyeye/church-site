e4aa901ad4f9e7edde34433d8d0fe8ac5a6e670a
Hezekiah Oyemade <oyedey@gmail.com>
2024-03-26 16:32:20 +0100

    refactor route request as query

--- node-api/admin/adminRoutes/routes.js
+++ node-api/admin/adminRoutes/routes.js
@@ -30,11 +30,11 @@ router.get('/', (request, response, next) => {
 router.post('/upload', /* userAuth.authenticateRequest, */ async (request, response) => await uploadMessage(request, response));
 
 /** DELETE single message Entry ========================== */
-router.delete('/:partitionKey/:rowKey/:filename', /* userAuth.authenticateRequest, */ async (request, response) => await deleteMessage(request, response));
+router.delete('/delete', /* userAuth.authenticateRequest, */ async (request, response) => await deleteMessage(request, response));
 
 
 /** Update single message Entry ========================== */
-router.put('/update-entity/:partitionKey/:rowKey', /* userAuth.authenticateRequest, */ async (request, response) => await updateMessage(request, response));
+router.put('/update', /* userAuth.authenticateRequest, */ async (request, response) => await updateMessage(request, response));
 
 /** Read multiple message Entry ========================== */
 router.get('/dashboard', /* userAuth.authenticateRequest, */ async (request, response) => await resources(request, response));
