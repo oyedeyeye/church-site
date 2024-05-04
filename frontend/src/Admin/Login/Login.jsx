f422d95daa1c17ae7376460a257dfcba6284ec66
olamiji-8 <olamijisotomi@gmail.com>
2024-05-04 12:57:43 +0100

    Making some changes

--- frontend/src/Admin/Login/Login.jsx
+++ frontend/src/Admin/Login/Login.jsx
@@ -1,86 +1,82 @@
-
- import Logo from "../../Assets/SEPCAM Logo PNG 1.svg"
+import Logo from "../../Assets/SEPCAM Logo PNG 1.svg";
 
 export default function Login() {
   return (
-    <>
-    
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+        <div className="text-center">
           <img
-            className="mx-auto h-40 w-20"
+            className="mx-auto h-16 w-auto"
             src={Logo}
             alt="Your Company"
           />
-          <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-2 text-2xl font-bold leading-8 text-gray-900">
             Sign in 
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
               Username or email
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder='Input your username'
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            </label>
+            <div className="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder='Input your username'
+                autoComplete="email"
+                required
+                className="block w-full rounded-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 sm:text-sm focus:outline-none pl-3"
+              />
             </div>
+          </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder='Enter Your Password'
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder='Enter Your Password'
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 sm:text-sm focus:outline-none pl-3"
+              />
             </div>
+          </div>
 
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Login
-              </button>
-            </div>
-          </form>
+          <div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Login
+            </button>
+          </div>
+        </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            <div>
-               <p>Keep Me Logged In</p> 
-               <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-               Forgot password/username
-            </a>
-            <p>
-              Don't have an account? {' '}    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Sign up 
-            </a>
-            </p>
-            </div>
-
+        <div className="mt-6">
+          <p className="text-sm text-gray-600 text-center">
+            <span>Keep Me Logged In</span> 
+            <span className="block mt-2">
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Forgot password/username
+              </a>
+            </span>
+            <span className="block mt-2">
+              Don't have an account? {' '}
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Sign up 
+              </a>
+            </span>
           </p>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
--- frontend/src/Pages/Resourcesvideo.js
+++ frontend/src/Pages/Resourcesvideo.js
@@ -194,7 +194,7 @@ const ResourcesVideo = () => {
               <div className="d-flex justify-content-between align-items-center">
                 <p>{sermonDetails.preacher}</p>
                 <img
-                  src="/images/team-1.jpg"
+                  src="/images/1.jpeg"
                   alt=""
                   className="w-10 h-10 rounded-full"
                 />
