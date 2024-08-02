5fda0ee1d453c4ffc85e5f3b5f2d19955b5021e0
Hezekiah Oyemade <oyedey@gmail.com>
2024-08-02 01:12:22 +0100

    contact form Refactoring

--- node-api/routes/controllers/contactForm.js
+++ node-api/routes/controllers/contactForm.js
@@ -1,40 +1,39 @@
 const nodemailer = require('nodemailer');
-const emailValidator = require('email-validator');
+const { validate } = require('deep-email-validator');
 require('dotenv').config();
+
 // const spamFilter = require('spam-filter');
 const { EMAIL_ADDRESS, EMAIL_PASSWD } = process.env;
 
 const contactForm = async (request, response) => {
   try {
-    const { name, phone, email, message } = request.query;
+    const { name, phone, email, message } = request.body;
+
+    console.log(`
+      name: ${name}
+      phone: ${phone},
+      email: ${email},
+      message: ${message}
+      `);
 
     // Validate the required fields
     if (!name || !phone || !email || !message) {
-      return response.status(400).send({
-        error: 'All fields are required',
-      });
+      return response.status(400).send({ error: 'All fields are required' });
     }
 
     // Validate user's email address
-    if (!emailValidator.validate(email)) {
-      return response.status(400).send({
-        error: 'Invalid email address'
+    const emailValidation = await validate(email);
+    if (!emailValidation.valid) {
+      return response.status(400).send({ 
+        status: 'error',
+        message: 'Invalid email address. Please try again!',
+        reason: emailValidation.reason
       });
     }
-
-    // Filter out spam requests
-    // if (spamFilter.isSpam(request.body)) {
-      // return response.status(400).send({
-        // error: 'Spam detected'
-      // });
-    // }
 
     // Send email to the gmailAddress
     const transporter = nodemailer.createTransport({
       service: "Gmail",
-      // host: gt.EMAIL_HOST,
-      // port: gt.EMAIL_PORT,
-      // secure: true, // or 'STARTTLS'
       auth: {
         user: EMAIL_ADDRESS,
         pass: EMAIL_PASSWD
@@ -45,49 +44,32 @@ const contactForm = async (request, response) => {
       from: EMAIL_ADDRESS,
       to: EMAIL_ADDRESS,
       subject: `Contact Form Submission by ${name}`,
-      text: `Message: ${message}
-
-      Sender Detail
-      Name: ${name}
-      Phone No: ${phone}
-      Email: ${email}`
+      text: `Message: ${message}\nSender Detail\nName: ${name}\nPhone No: ${phone}\nEmail: ${email}`
     };
 
     transporter.sendMail(emailOptions, (error, info) => {
       if (error) {
-        return response.status(500).send({
-          message: 'Failed to send mail',
-          error: error.message,
-        });
+        return response.status(500).send({ message: 'Failed to send mail', error: error.message });
       }
 
-    // Send Acknowledgement email to the user
-    const acknowledgementMailOptions = {
-      from: EMAIL_ADDRESS,
-      to: email,
-      subject: 'Acknowledgement: Contact Form Submission',
-      text: `Dear ${name},
+      // Send Acknowledgement email to the user
+      const acknowledgementMailOptions = {
+        from: EMAIL_ADDRESS,
+        to: email,
+        subject: 'Acknowledgement: Contact Form Submission',
+        text: `Dear ${name},\nThank you for reaching out to us!\nWe will reachout to you as soon as possible.\n\nBest Regards,\nSepcam Media Team`
+      };
 
-      Thank you for reaching out to us!
-      We will reachout to you as soon as possible.
-      
-      Best Regards,
-      Sepcam Media Team`
-    };
+      transporter.sendMail(acknowledgementMailOptions, (ackError, ackInfo) => {
+        if (ackError) {
+          console.error(ackError);
+        }
+      });
 
-    transporter.sendMail(acknowledgementMailOptions, (ackError, ackInfo) =>{
-      if (ackError) {
-        console.error(ackError);
-      }
+      return response.send({ message: 'Email sent successfully' });
     });
-
-    return response.send({ message: 'Email sent successfully' });
-  });
-
   } catch (error) {
-    response.status(500).json({
-      error: error.message
-    });
+    response.status(500).json({ error: error.message });
   }
 };
 
